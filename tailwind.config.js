19591e853783219baef6065bf67068c7de7e735c
waad <wd2288402@gmail.com>
2025-01-01 23:29:23 +0300

    Your commit message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  // Specify the paths to all of the template files in your project
+  content: [
+    './src/**/*.{html,js,ts,jsx,tsx}', // Adjust the paths to match your project structure
+    './src/screens/templates/**/*.html', // Path to HTML templates inside the screens/templates folder
+  ],
+  // Extend the default theme
   theme: {
-    extend: {},
+    extend: {
+      // Add custom colors
+      colors: {
+        customGreen: '#00A550',
+        lightGreen: '#DFFFD6',
+        pastelYellow: '#FFFACD',
+      },
+      // Add custom fonts
+      fontFamily: {
+        pacifico: ['Pacifico', 'cursive'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
   },
+  // Add any plugins you want to use
   plugins: [],
-}
-
+};
